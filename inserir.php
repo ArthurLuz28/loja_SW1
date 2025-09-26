f1261fb613f1656c2535b02cbf070bcb38ba85c3
ArthurLuz28 <arthurluz728@gmail.com>
2025-09-26 19:13:34 -0300

    inserir update

--- inserir.php
+++ inserir.php
@@ -1,28 +1,39 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lojinha ETECMCM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 
-<?php
-    require 'conexao.php';
+    <?php
+        require 'conexao.php';
 
-    $nome = $_POST['nome'];
-    $preco = $_POST['preco'];
-    $quantidade = $_POST['quantidade'];
+        $nome = $_POST['nome'];
+        $preco = $_POST['preco'];
+        $quantidade = $_POST['quantidade'];
 
-    $sql = "INSERT INTO produtos (nome, preco, quantidade) VALUES (:nome, :preco, :quantidade)";
+        $sql = "INSERT INTO produtos (nome, preco, quantidade) VALUES (:nome, :preco, :quantidade)";
 
-    $stmt = $pdo->prepare($sql);
+        $stmt = $pdo->prepare($sql);
 
-    $stmt->bindParam(':nome', $nome);
-    $stmt->bindParam(':preco', $preco);
-    $stmt->bindParam(':quantidade', $quantidade);
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':preco', $preco);
+        $stmt->bindParam(':quantidade', $quantidade);
 
-    if ($stmt->execute()) {
-        echo "Produto inserido com sucesso!";
-    } else {
-        echo "Erro ao inserir produto.";
-    }
-    
-?>
-    <a href="index.php">
-        <button type="button" class="btn btn-primary">Voltar</button>
-    </a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+        if ($stmt->execute()) {
+            echo "<div class='alert alert-success'>Produto inserido com sucesso!</div>";
+        } else {
+            echo "<div class='alert alert-danger'>Erro ao inserir produto.</div>";
+        }
+        
+    ?>
+        <a href="index.php">
+            <button type="button" class="btn btn-primary">Voltar</button>
+        </a>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
